ed551a3cf584358c32e0d0d713c5456e4010a7a0
MrBearTW <jamieperng@gmail.com>
2023-01-12 18:05:01 -0500

    Workshop - #1

---
+++ S1/IPC144/Workshop1/Workshop1/w1p1.c
@@ -0,0 +1,39 @@
+/*///////////////////////////////////////////////////////////////////////////
+                              Reflection
+                             Workshop - #1
+Full Name  : Chi-Wei Perng
+Student ID#: 121967228
+Email      : cperng@myseneca.ca
+Section    : NCC
+
+Authenticity Declaration:
+
+I declare this submission is the result of my own work and has not been
+shared with any other student or 3rd party content provider. This submitted
+piece of work is entirely of my own creation.
+///////////////////////////////////////////////////////////////////////////*/
+
+
+#include <stdio.h>
+
+int main(void)
+{
+	printf("Workshop 1 Part-1\n");
+	printf("=================\n");
+	printf("\n");
+	printf("I'm displaying this using the \'printf\' stdio\n");
+	printf("(standard input output) library function!\n");
+	printf("\n");
+	printf("Dear teacher,\n");
+	printf("\n");
+	printf("  I promise I will work hard from this day onward.\n");
+	printf("  I acknowledge that practice is extremely important,\n");
+	printf("  so I will do all workshops, quizzes, and assignments.\n");
+	printf("\n");
+	printf("Sincerely,\n");
+	printf("\n");
+	printf("Chi-Wei Perng\n");
+	printf("Student ID# 121967228\n");
+
+	return 0;
+}
---
+++ S1/IPC144/Workshop1/Workshop1/w1p2.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+int main(void)
+{
+    printf("Workshop 1 Part-2\n");
+    printf("=================\n");
+    printf("\n");
+    printf("Using tab specifiers...\n");
+    printf("\tTab-1\tTab-2\tTab-3\n");
+    printf("\t-----\t=====\t-----\n");
+    printf("\n");
+    printf("\\ this is a back-slash character!\n");
+    printf("%% this is a percent sign character!\n");
+    printf("\" this is a double-quote character!\n");
+    printf("\n");
+    printf("My favourite quotes are:\n");
+    printf("1.\t\"Skill is only developed by hours and hours of work.\"\n");
+    printf("\t\t\t\t\t\t-Usain Bolt\n");
+    printf("\n");
+    printf("2.\t\"It's not about having time. It's about making time.\"\n");
+    printf("\t\t\t\t\t\t-unknown\n");
+    printf("\n");
+    printf("3.\t\"All of us do not have equal talent. But, all of us\n");
+    printf("\thave an equal opportunity to develop our talents.\"\n");
+    printf("\t\t\t\t\t\t-A.P.J. Abdul Kalam\n");
+
+    return 0;
+}
