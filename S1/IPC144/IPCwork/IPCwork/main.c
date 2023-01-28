f706c321bf4367ab0b0ff9ab1411f867290e1d6b
MrBearTW <jamieperng@gmail.com>
2023-01-27 23:17:13 -0500

    20230127 IPC144-W3-P1

---
+++ S1/IPC144/IPCwork/IPCwork/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  IPCwork
+//
+//  Created by Chi-Wei Perng on 2023-01-26.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
