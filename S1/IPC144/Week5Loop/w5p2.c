513edfce7d2d166f0e37284bb20f2e7f2d6daace
MrBearTW <jamieperng@gmail.com>
2023-02-13 00:34:40 -0500

    IPC144 w5p2

--- S1/IPC144/Week5Loop/w5p2.c
+++ S1/IPC144/Week5Loop/w5p2.c
@@ -42,54 +42,11 @@ int main(void)
     printf("\n");
     printf("*** Log date set! ***\n");
     printf("\n");
-    printf("Log starting date: %d-", year);
-    switch (month)
-    {
-    case 1:
-        printf("JAN");
-        break;
-    case 2:
-        printf("FEB");
-        break;
-    case 3:
-        printf("MAR");
-        break;
-    case 4:
-        printf("APR");
-        break;
-    case 5:
-        printf("MAY");
-        break;
-    case 6:
-        printf("JUN");
-        break;
-    case 7:
-        printf("JUL");
-        break;
-    case 8:
-        printf("AUG");
-        break;
-    case 9:
-        printf("SEP");
-        break;
-    case 10:
-        printf("OCT");
-        break;
-    case 11:
-        printf("NOV");
-        break;
-    case 12:
-        printf("DEC");
-        break;
-    default:
-        break;
-    }
-    printf("-01\n");
 
     // part 2:
     for (i = 0; i < LOG_DAYS; i++)
     {
-        printf("Log starting date: %d-", year);
+        printf("%d-", year);
         switch (month)
         {
         case 1:
