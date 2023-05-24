3eb9205fe09fb174bee9bee68cda2951d6b1b047
MrBearTW <jamieperng@gmail.com>
2023-05-24 10:02:29 -0400

    OOP244 Workshop 1 Part2 DIY

---
+++ S2/WEB222/week3.js
@@ -0,0 +1,18 @@
+var x = 1;
+
+function parent() {
+    var x = 2;
+
+    function child() {
+        console.log(x);
+    }
+
+    child();
+}
+parent()
+
+
+var s3 = new String("Some Text");
+
+JSON.stringify
+
