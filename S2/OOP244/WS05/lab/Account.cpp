b6d7d755f78ac00f80ddb5f071917d562c4c46c5
MrBearTW <jamieperng@gmail.com>
2023-06-15 23:54:42 -0400

    OOP Workshop5 Part1

--- S2/OOP244/WS05/lab/Account.cpp
+++ S2/OOP244/WS05/lab/Account.cpp
@@ -1,3 +1,13 @@
+/*/////////////////////////////////////////////////////////////////////////
+                            Workshop 5 Part 1
+Full Name  :CHI-WEI PERNG
+Email      :cperng@myseneca.ca
+Student ID#:121967228
+Section    :NGG
+Date       :2023/06/15
+
+/////////////////////////////////////////////////////////////////////////*/
+
 #define _CRT_SECURE_NO_WARNINGS
 #include <iostream>
 #include "Account.h"
@@ -51,4 +61,150 @@ namespace sdds
       return cout;
    }
 
+   // -------------------- type conversion operators --------------------
+
+   // operator bool
+   Account::operator bool() const
+   {
+      return m_number >= 10000 && m_number <= 99999 && m_balance >= 0;
+   }
+
+   // operator int
+   Account::operator int() const
+   {
+      return m_number;
+   }
+
+   // operator double
+   Account::operator double() const
+   {
+      return m_balance;
+   }
+
+   // -------------------- Unary member operator --------------------
+
+   bool Account::operator~() const
+   {
+      return m_number == 0;
+   }
+
+   //  -------------------- Binary member operator --------------------
+
+   // assignment operators
+   Account &Account::operator=(int number)
+   {
+      // If the integer value is an invalid account number, the object should be set to an invalid empty state instead.
+      if (number < 10000 || number > 99999)
+      {
+         setEmpty();
+      }
+
+      // If the account is not new, then this operator should not perform any action.
+      if (~*this)
+      {
+         m_number = number;
+      }
+
+      // In any case, a reference of the current object (account) should be returned.
+      return *this;
+   }
+
+   // overload the assignment operator
+   Account &Account::operator=(Account &left)
+   {
+      // This action should move the balance and the account number from one account to another; therefore unlike the usual assignment operator that affects the left operand only, this operator will affect both operands;
+      // the balance of the left account will be set to the balance of the right account and then the balance of the right account will be set to zero.
+      // The same will happen to the account number If the left Account operand is not new or the right account operand is not valid, no action should be taken.
+      if (~*this && left)
+      {
+         m_balance = left.m_balance;
+         left.m_balance = 0.0;
+         m_number = left.m_number;
+         left.m_number = 0;
+      }
+
+      // In any case, a reference of the current object (account) should be returned.
+      return *this;
+   }
+
+   // overload the += operator
+   Account &Account::operator+=(double deposit)
+   {
+      // This should act like depositing money into an account. (i.e. the value of the double should be added to the balance)
+      // if the account is in an invalid state or the double value is negative, no action should be taken.
+      if (*this && deposit > 0)
+      {
+         m_balance += deposit;
+      }
+      // In any case, a reference of the current object (account) should be returned.
+      return *this;
+   }
+
+   // overload the -= operator
+   Account &Account::operator-=(double withdraw)
+   {
+      // This should act like withdrawing money from an account. (i.e. the value of the balance should be reduced by the double value)
+      // if the account is in an invalid state, the double value is negative or there is not enough money in the account no action should be taken.
+      if (*this && withdraw > 0 && m_balance >= withdraw)
+      {
+         m_balance -= withdraw;
+      }
+
+      // In any case, a reference of the current object (account) should be returned.
+      return *this;
+   }
+
+   // overload the << operator (left shift operator)
+   Account &Account::operator<<(Account &moveToLeft)
+   {
+      // After this operation, the balance of the left account will be the sum of both and the balance of the right account will be zero.
+      // Funds of an account should not be able to be moved to itself and this operation does not affect the account.
+      if (*this && moveToLeft && this != &moveToLeft)
+      {
+         this->m_balance += moveToLeft.m_balance;
+         moveToLeft.m_balance = 0.0;
+      }
+
+      // In any case, a reference of the current object (account) should be returned.
+      return *this;
+   }
+
+   // overload the >> operator (right shift operator)
+   Account &Account::operator>>(Account &moveToRight)
+   {
+      // After this operation, the balance of the right account will be the sum of both and the balance of the left account will be zero.
+      // Funds of an account should not be able to be moved to itself and this operation does not affect the account.
+      if (*this && moveToRight && this != &moveToRight)
+      {
+         moveToRight.m_balance += this->m_balance;
+         this->m_balance = 0.0;
+      }
+
+      // In any case, a reference of the current object (account) should be returned.
+      return *this;
+   }
+
+   //  -------------------- Binary helper operators --------------------
+
+   // The double value should be the sum of the balances of the two accounts.
+   // If any of the two accounts is invalid, then zero is returned.
+   double operator+(const Account &left, const Account &right)
+   {
+      double sum = 0.0;
+      if (left && right)
+      {
+         sum = left.m_balance + right.m_balance;
+      }
+      return sum;
+   }
+
+   // The value of the balance of the right operand (account reference) should be added to the left operand (double reference)
+   double operator+=(double &left, const Account &right)
+   {
+      if (right)
+      {
+         left += right.m_balance;
+      }
+      return left;
+   }
 }
--- S2/OOP244/WS05/lab/Account.h
+++ S2/OOP244/WS05/lab/Account.h
@@ -1,3 +1,13 @@
+/*/////////////////////////////////////////////////////////////////////////
+                            Workshop 5 Part 1
+Full Name  :CHI-WEI PERNG
+Email      :cperng@myseneca.ca
+Student ID#:121967228
+Section    :NGG
+Date       :2023/06/15
+
+/////////////////////////////////////////////////////////////////////////*/
+
 #ifndef SDDS_ACCOUNT_H_
 #define SDDS_ACCOUNT_H_
 #include <iostream>
@@ -14,6 +24,54 @@ namespace sdds
       Account();
       Account(int number, double balance);
       std::ostream &display() const;
+
+      // -------------------- type conversion operators --------------------
+
+      // operator bool
+      // returns true if the account is valid and otherwise false. This operator can not modify the account object.
+      operator bool() const;
+
+      // operator int
+      // returns the account number. This operator can not modify the account object.
+      operator int() const;
+
+      // operator double
+      // returns the balance value. This operator can not modify the account object.
+      operator double() const;
+
+      // -------------------- Unary member operator --------------------
+
+      // This operator returns true is the account is new or not set (i.e. if the account number is zero), otherwise it will return false. This operator can not modify the account object.
+      bool operator~() const;
+
+      //  -------------------- Binary member operator --------------------
+
+      //  ssignment operators
+      // overload the assignment operator so a NEW account can be set to an integer. Doing so should set the account number of the account to the integer value.
+      Account &operator=(int number);
+
+      // overload the assignment operator so a NEW account can be set to another Account object.
+      Account &operator=(Account &left);
+
+      // overload the += operator to add a double value to an account. This should act like depositing money into an account.
+      Account &operator+=(double deposit);
+
+      // overload the -= operator to reduce an account balance by a double value . This should act like withdrawing money from an account.
+      Account &operator-=(double withdraw);
+
+      // overload the << operator (left shift operator) to move funds from the right account to the left.
+      Account &operator<<(Account &moveToLeft);
+
+      // overload the >> operator (right shift operator) to move funds from the left account to the right.
+      Account &operator>>(Account &moveToRight);
+
+      //  -------------------- Binary helper operators --------------------
+
+      // create a binary stand alone helper + operator that accepts a constant account reference at left and another constant account reference at right and returns a double value.
+      friend double operator+(const Account &left, const Account &right);
+
+      // create a binary stand alone helper += operator that accepts a double reference at left and a constant account reference at right and returns a double value.
+      friend double operator+=(double &left, const Account &right);
    };
 
 }
